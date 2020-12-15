b0896b12e37b6a33217bceb8f34a579d2b66b0b1
Zohaib <zohaibk1111@gmail.com>
2020-12-15 03:14:57 -0500

    binarySearch completed

--- binarSearch/binarySearch.js
+++ binarSearch/binarySearch.js
@@ -1,15 +1,24 @@
-const binarySearch = (start, end, searchVal, arr) => {
+const binarySearch = (arr, x, start, end) => {
   if (start > end) {
     return false;
   }
 
   let midVal = Math.floor((start + end) / 2);
 
-  if (arr[midVal] === searchVal) {
+  if (arr[midVal] === x) {
+    let index = arr.indexOf(x);
+    console.log(index);
+    console.log("Found Element! it is located at index: " + index);
     return true;
   }
 
-  if (arr[midVal] > searchVal) {
-    return binarySearch(start, midVal - 1, searchVal, arr);
+  if (arr[midVal] > x) {
+    return binarySearch(arr, x, start, midVal - 1);
+  } else {
+    return binarySearch(arr, x, midVal + 1, end);
   }
 };
+
+let array = [1, 3, 6, 7, 9, 10, 15];
+
+console.log(binarySearch(array, 9, 0, array.length - 1));
